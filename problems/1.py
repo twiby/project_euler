576823984f8cacd8da79fd16e420554ad429ca70
twiby <t.balenbois@gmail.com>
2023-02-25 18:31:46 +0100

    problem 1

--- problems/1.py
+++ problems/1.py
@@ -1,5 +1,18 @@
+def sum_multiples_of_k(k_list, N):
+	sum  = {k:0 for k in k_list}
+	count = [0 for _ in k_list]
+	for i in range(1, N):
+		for k in range(len(k_list)):
+			count[k] += 1
+			if count[k] == k_list[k]:
+				sum[k_list[k]] += i
+				count[k] = 0
+	return sum
+
 def main():
-	return 1
+	N = 1000
+	sums = sum_multiples_of_k([3, 5, 15], N)
+	return sums[3] + sums[5] - sums[15]
 
 if __name__ == "__main__":
 	print(main())
