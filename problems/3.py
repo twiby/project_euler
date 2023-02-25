d7efbe1e2c8be17c6b42515d2c089b7eee38b3e2
twiby <t.balenbois@gmail.com>
2023-02-25 18:42:01 +0100

    problem 3

--- problems/3.py
+++ problems/3.py
@@ -1,5 +1,24 @@
+import numpy as np
+
+class PrimeException(Exception):
+	pass
+
+def partial_factorize(n):
+	for i in range(2, int(np.sqrt(n)) + 1):
+		res = n / i
+		if res == np.floor(res):
+			return i, n/i
+	raise PrimeException
+
+def factorize(n, top=True):
+	try:
+		a, b = partial_factorize(n)
+		return *factorize(a, False), *factorize(b, False)
+	except PrimeException:
+		return (int(n),)
+
 def main():
-	return 3
+	return np.max(factorize(600851475143))
 
 if __name__ == "__main__":
 	print(main())
