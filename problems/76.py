5bdbd4f485ff279a3cfbf94e244a8326c15024cc
twiby <t.balenbois@gmail.com>
2023-03-22 16:11:25 +0100

    problem 76

---
+++ problems/76.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def main():
+	N = 5
+	N = 100
+	values = [i for i in range(1, N)]
+	values.reverse()
+
+	table = np.zeros((N+1,N+1), dtype=np.int64)
+	table[0,:] = 1
+	table[1,:] = 1
+
+	for i in range(2, N+1):
+		table[i,1] = 1
+		for j in range(2, i):
+			table[i,j] = table[i::-j, j-1].sum()
+		table[i, i:] = table [i, i-1] + 1
+	return table[N, N-1]
+
+if __name__=="__main__":
+	print(main())
